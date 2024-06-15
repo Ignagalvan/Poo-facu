133938246de8a7970d8174953d45faaf1c648a9e
Ignagalvan <ignad.galvan@gmail.com>
2024-06-15 18:56:06 -0300

    README1

---
+++ Ejercicio 17/login.h
@@ -0,0 +1,40 @@
+#ifndef LOGIN_H
+#define LOGIN_H
+
+#include <QWidget>
+#include <QNetworkAccessManager>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+
+class Login : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Login(QWidget *parent = nullptr);
+
+protected:
+    void paintEvent(QPaintEvent *) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+
+private slots:
+    void slot_validarUsuario();
+    void slot_descargaFinalizada(QNetworkReply *reply);
+
+private:
+    QImage im;
+    QLabel *lUsuario;
+    QLabel *lClave;
+    QLineEdit *leUsuario;
+    QLineEdit *leClave;
+    QPushButton *pbIngresar;
+    QNetworkAccessManager *manager;
+    QPoint mousePressPos;
+    QPoint widgetPos;
+    bool resize;
+};
+
+#endif // LOGIN_H
